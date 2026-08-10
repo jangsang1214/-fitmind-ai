3a6018a480c1621149dde0705954936de97b63dd
jangsang1214 <jangsang051214@gmail.com>
2026-08-10 18:37:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE="fitmind-cache-v2";const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest"];
+const CACHE="fitmind-cache-v1";const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
