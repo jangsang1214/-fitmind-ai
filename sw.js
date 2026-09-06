2e6ff92adc825da58aedc99f64523049379aa00b
jangsang1214 <jangsang051214@gmail.com>
2026-09-06 12:14:13 +0900

    Rotate PWA shell for Firebase auth recovery

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader mobile-layout-recovery-v8-20260906 */
-importScripts('./02_core/sw-runtime.js?v=mobile-layout-recovery-v8-20260906');
+/* GARANG SW loader firebase-auth-recovery-v9-20260906 */
+importScripts('./02_core/sw-runtime.js?v=firebase-auth-recovery-v9-20260906');
