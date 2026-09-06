df4c886b9796ebc51bbd7a0be5242fc5863cb092
jangsang1214 <jangsang051214@gmail.com>
2026-09-06 23:24:53 +0900

    chore(sw): activate Coach touch-fix cache

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader app-shell-v15-20260906 */
-importScripts('./02_core/sw-runtime.js?v=app-shell-v15-20260906');
+/* GARANG SW loader app-shell-v16-20260906 */
+importScripts('./02_core/sw-runtime.js?v=app-shell-v16-20260906');
