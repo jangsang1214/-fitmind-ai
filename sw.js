56e7de92bfc6ae56a6e0d502aaf264e4caa1b7cf
jangsang1214 <jangsang051214@gmail.com>
2026-09-06 22:48:51 +0900

    chore(sw): activate Coach shell cache generation

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader app-shell-v14-20260906 */
-importScripts('./02_core/sw-runtime.js?v=app-shell-v14-20260906');
+/* GARANG SW loader app-shell-v15-20260906-coach-shell */
+importScripts('./02_core/sw-runtime.js?v=app-shell-v15-20260906-coach-shell');
