302832c0252ec76f6450c294058af33c96d50d33
jangsang1214 <jangsang051214@gmail.com>
2026-09-06 15:08:26 +0900

    Align iOS hotfix service worker version contract

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader app-shell-v13-20260906-ios */
-importScripts('./02_core/sw-runtime.js?v=app-shell-v13-20260906-ios');
+/* GARANG SW loader app-shell-v13-20260906 */
+importScripts('./02_core/sw-runtime.js?v=app-shell-v13-20260906');
