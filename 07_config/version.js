d17be3d347bcd4d8f1f5d831b28f61df94ad5e21
jangsang1214 <jangsang051214@gmail.com>
2026-09-06 13:42:00 +0900

    Align browser build version with app UI

--- 07_config/version.js
+++ 07_config/version.js
@@ -1,6 +1,6 @@
 (function(root){
  'use strict';
- const build=Object.freeze({version:'0.11.0-beta.1',label:'0.11.0 BETA 1',channel:'TODAY + Coach Engine V0',schema:1});
+ const build=Object.freeze({version:'0.11.0-beta.5',label:'0.11.0 BETA 5',channel:'STABILITY + DATA V2',schema:5});
  root.GARANG_BUILD=build;
  if(root.document){document.documentElement.dataset.garangVersion=build.version;document.documentElement.dataset.garangChannel=build.channel;}
 })(typeof window==='undefined'?globalThis:window);
