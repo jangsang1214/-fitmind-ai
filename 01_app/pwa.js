049de3c3c88026900f481053b44026fa2a44b671
jangsang1214 <jangsang051214@gmail.com>
2026-09-07 16:41:34 +0900

    fix(pwa): bypass stale worker cache on registration

--- 01_app/pwa.js
+++ 01_app/pwa.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator&&location.protocol!=='file:'){
- navigator.serviceWorker.register(new URL('../sw.js',document.currentScript.src),{scope:new URL('../',document.currentScript.src).pathname}).catch(error=>{
+ navigator.serviceWorker.register(new URL('../sw.js',document.currentScript.src),{scope:new URL('../',document.currentScript.src).pathname,updateViaCache:'none'}).then(registration=>registration.update()).catch(error=>{
   console.warn('Offline shell unavailable:',error.message);
  });
 }
