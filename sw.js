60be81a43e9f5e6c40e0c4bf21241b9ca4be19ba
jangsang1214 <jangsang051214@gmail.com>
2026-09-15 18:53:05 +0900

    Rotate GARANG service worker loader for final integration

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader app-shell-v26-20260915 */
-importScripts('./02_core/sw-runtime.js?v=app-shell-v26-20260915');
+/* GARANG SW loader app-shell-v27-20260915 */
+importScripts('./02_core/sw-runtime.js?v=app-shell-v27-20260915');
