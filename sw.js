849b3895e2a97c963c78caeeb948ba5af2070b92
jangsang1214 <jangsang051214@gmail.com>
2026-09-15 17:07:45 +0900

    Align service worker loader with app shell v24

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader app-shell-v23-20260915 */
-importScripts('./02_core/sw-runtime.js?v=app-shell-v23-20260915');
+/* GARANG SW loader app-shell-v24-20260915 */
+importScripts('./02_core/sw-runtime.js?v=app-shell-v24-20260915');
