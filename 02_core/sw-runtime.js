29639878be70474805affe26d3745072880271d1
jangsang1214 <jangsang051214@gmail.com>
2026-09-15 16:55:39 +0900

    Precache state durability runtime

--- 02_core/sw-runtime.js
+++ 02_core/sw-runtime.js
@@ -1,6 +1,9 @@
 const CACHE_PREFIX='garang-app-shell-';
-const CACHE=`${CACHE_PREFIX}v22-20260915`;
-const EXTRA_ASSETS=['./06_features/ui/runtime/garang-today-checkin-override-v1.js?v=1.3.0'];
+const CACHE=`${CACHE_PREFIX}v23-20260915`;
+const EXTRA_ASSETS=[
+  './06_features/ui/runtime/garang-state-event-durability-v1.js?v=1.0.0',
+  './06_features/ui/runtime/garang-today-checkin-override-v1.js?v=1.3.0'
+];
 
 async function precache(){
   const cache=await caches.open(CACHE);
