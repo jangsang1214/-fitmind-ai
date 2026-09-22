d1134fb7fcf54b6bbac7fe3422e2c6d4b075c2c8
jangsang1214 <jangsang051214@gmail.com>
2026-09-22 12:02:47 +0900

    Bump commercial hardening build identity

--- 07_config/version.js
+++ 07_config/version.js
@@ -1,6 +1,6 @@
 (function(root){
  'use strict';
- const build=Object.freeze({version:'0.11.0-beta.5',label:'0.11.0 BETA 5',channel:'STABILITY + DATA V2',schema:5});
+ const build=Object.freeze({version:'0.11.0-beta.6',label:'0.11.0 BETA 6',channel:'COMMERCIAL HARDENING',buildId:'20260922-y2g-1',schema:5});
  root.GARANG_BUILD=build;
  if(root.document){document.documentElement.dataset.garangVersion=build.version;document.documentElement.dataset.garangChannel=build.channel;}
 })(typeof window==='undefined'?globalThis:window);
