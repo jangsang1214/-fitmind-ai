647a651c447da6c2c4e8ea3d74a5c4ce13f9f67c
jangsang1214 <jangsang051214@gmail.com>
2026-09-25 10:20:51 +0900

    Rotate service worker loader for Body v7.2

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* GARANG SW loader app-shell-v31-20260925 */
-importScripts('./02_core/sw-runtime.js?v=app-shell-v31-20260925');
+/* GARANG SW loader app-shell-v32-20260925 */
+importScripts('./02_core/sw-runtime.js?v=app-shell-v32-20260925');
