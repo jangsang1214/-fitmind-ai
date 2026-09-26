64e28fa5c1cb3f36496ffcc01b25b6b0f84d657f
jangsang1214 <jangsang051214@gmail.com>
2026-09-27 02:00:15 +0900

    Port Body v7.2: 02_core/sw-runtime.js

--- 02_core/sw-runtime.js
+++ 02_core/sw-runtime.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='garang-app-shell-';
-const CACHE=`${CACHE_PREFIX}v31-20260925`;
+const CACHE=`${CACHE_PREFIX}v32-20260925`;
 const EXTRA_ASSETS=[
   './06_features/ui/runtime/garang-state-event-durability-v1.js?v=1.1.0',
   './06_features/ui/runtime/garang-today-checkin-override-v1.js?v=1.3.0',
